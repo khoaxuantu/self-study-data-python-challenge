a7927c88645879aa65bb2ae18a15bf1e34e1756d
Nguyen Tran Duy Anh <nguyenanhkt9205@gmail.com>
2024-08-06 10:32:10 +0700

    Them bai cua voi, thocao, baodk

--- project2/baodk.py
+++ project2/baodk.py
@@ -1,13 +1,31 @@
 class Operator:
-  def __init__(self, name, apply_func=None, precedence=0):
-    self.name = name
+  def __init__(self, sign, apply_func=None, precedence=0):
+    self.sign = sign
     self.apply = apply_func
     self.precedence = precedence
+  def __str__(self):
+    return self.sign
+  def __repr__(self):
+    return f"Op({self.sign})"
+
+def modulo(a, b):
+  if isinstance(b, int):
+    return a%b
+  if isinstance(b, float):
+    if int(b)!=b:
+      raise ValueError(f"{b} is not integer")
+  return a%int(b)
+
+class Number(float):
+  def __str__(self):
+    s = f"{self:f}"
+    s = s.rstrip('0').rstrip('.')
+    return s
 
 class Calculator:
   # Declare the functions for all allowed operators
   operators = {"+": Operator("+", lambda a,b: a + b, 1)
-              ,"-": Operator("-", lambda a,b: a - b, 1)
+              ,"-": Operator("-", precedence=1)
               ,"x": Operator("x", lambda a,b: a * b, 2)
               ,"/": Operator("/", lambda a,b: a / b, 2)
               ,"%": Operator("%", lambda a,b: a % b, 3)}
@@ -15,83 +33,127 @@ class Calculator:
   def __init__(self):
     pass
 
-  def get_operator(self, op):
-    """
-    The function return operator object.
-    """
-    return self.operators.get(op)
+  def get_operator(self, sign):
+    """The function return operator object."""
+    return self.operators.get(sign)
 
-  def is_operator(self, op):
-    """
-    The function returns op string is an operator or not.
-    """
-    return self.get_operator(op) is not None
+  def is_operator(self, sign):
+    """The function returns op string is an operator or not."""
+    return bool(sign in self.operators.keys())
+  
+  def no_minus(self, factors):
+    it = 0
+    length = len(factors)
+    while it<length:
+      factor = factors[it]
+      if isinstance(factor, Operator):
+        if factor.sign == '-' and it<length and isinstance(factors[it+1], Number):
+          factors[it+1] = Number(-factors[it+1])
+          factors.pop(it)
+          length-=1
+          if it>0 and not isinstance(factors[it-1], Operator):
+            factors.insert(it, self.get_operator('+'))
+            length+=1
+          continue
+        if it==0 and factor.precedence>1:
+          raise SyntaxError(f"Invalid input: expression might not starts with '{factor}'!")
+      it+=1
+    return factors
 
+  def factoring(self, expr):
+    """The function is used to parse numbers, operators, opens, closes from an expression."""
+    it = 0
+    factors = []
+    length = len(expr)
+    while it<length:
+      char=expr[it]
+      if char==' ':
+        pass
+      elif self.is_operator(char):
+        factors.append(self.get_operator(char))
+      elif char in ('(', ')'):
+        factors.append(char)
+      elif char.isdigit() or char=='.':
+        start=it
+        while it<length and (char.isdigit() or char=='.' or char==' '):
+          it+=1
+          if it<length: char=expr[it]
+        numStr = expr[start:it].strip()
+        if not numStr.replace('.', '', 1).isnumeric():
+          raise SyntaxError(f"Invalid input: '{numStr}' is not a valid number!")
+        factors.append(Number(numStr))
+        it-=1
+      else:
+        raise SyntaxError(f"Invalid input: '{expr[it]}' is invalid input expression!")
+      it+=1
+    return self.no_minus(factors)
+  
   def evaluate(self, expression):
     """
     This is the core function that evaluates a mathematical expression given as a string.
     """
-    i, stack = 0, []
-    expression = expression.lower()
-    while i<len(expression):
-      # handling space characters
-      if expression[i]==' ':
-        i += 1
-        continue
-      # handling group open by recursive and multiply op
-      elif expression[i]=='(':
-        val, j = self.evaluate(expression[i+1:])
-        op = self.get_operator('x')
-        if isinstance(stack[-1], Operator):
-          op = stack.pop()
-        stack.append(op.apply(stack.pop(), val))
-        i += j
-      # handling digits
-      elif expression[i].isdigit() or expression[i] == '.':
-        start = i
-        decimalCount = 0
-        # handling decimal points in number string
-        while (i<len(expression) and 
-          (expression[i].isdigit() or expression[i] == '.' or expression[i] == ' ')):
-          if expression[i] == '.':
-            decimalCount += 1 
-          if decimalCount > 1:
-            raise SyntaxError('Invalid input: more than one decimal point in number string!')
-          i+=1
-        # handling space inside number string
-        numStr = expression[start:i]
-        if ' ' in numStr.strip():
-          raise SyntaxError('Invalid input: space found within number string!')
-        # add number into stack
-        val = float(numStr)
-        if len(stack)==0:
-          stack.append(val)
-        elif isinstance(stack[-1], Operator):
-          op = stack.pop()
-          stack.append(op.apply(stack.pop(), val))
-        # handling number with number without operator
-        else:
-          raise SyntaxError('Invalid input: number following group without operator!')
-        i-=1
-      # handling closing group
-      elif expression[i]==')':
-        return stack[-1], i+1
-      # check & add operator into stack
-      elif self.is_operator(expression[i]):
-        op = self.get_operator(expression[i])
-        stack.append(op)
-      # unspecified character
-      else:
-        raise SyntaxError(f"Invalid input: '{expression[i]}' is invalid input expression!")
-      i+=1
-    
+    factors = self.factoring(expression.lower())
 
-    return stack[-1]
+    def apply_op_from_left(ops, values):
+      """This sub-function is used to apply operators from left to right once
+      that expression has no higher precedence inside."""
+      op = ops.pop(0)
+      lVal, _ = values.pop(0)
+      rVal, _ = values.pop(0)
+      res = Number(op.apply(lVal, rVal))
+      print(f"apply: {lVal}{op}{rVal}={res}") # print for debugging
+      values.insert(0, (res, _))
+
+    def sub_evaluate(facts, start_prec=1):
+      """This sub-function supports grouping and higher precedence operating."""
+      values, ops = [], []
+      it = 0
+      length = len(facts)
+      while it<length:
+        fact = facts[it]
+        # print(f">expr: {' '.join(map(str,facts))}") # debug expr print
+        # print(f"[sub]: fact={fact},ops={ops},vals={values},it={it},prec={start_prec}") # debug iterator print
+        if isinstance(fact, Number):
+          if it>1 and facts[it-1]==')':
+            raise SyntaxError(f"Invalid input: number '{fact}' follows group without operator!")
+          values.append((fact, it))
+        elif isinstance(fact, Operator):
+          if fact.precedence>start_prec:
+            # start child eval from previous number iterator
+            _, idx = values.pop()
+            subVal, subLen = sub_evaluate(facts[idx:], start_prec=fact.precedence)
+            it += subLen-2
+            values.append((subVal, it-1))
+          elif start_prec>fact.precedence:
+            # break point for higher precedence handling
+            break
+          else: ops.append(fact)
+        elif fact=='(':
+          # add 'x' operator if before '(' have no operator
+          if it>0 and not isinstance(facts[it-1], Operator):
+            facts.insert(it, self.get_operator('x'))
+            length+=1
+            continue
+          # start child eval from iterator + 1
+          subVal, subLen = sub_evaluate(facts[it+1:])
+          it += subLen+1
+          values.append((subVal, it))
+        elif fact==')':
+          # break point for closing group
+          break
+        it+=1
+        
+      # same precedences handling, perform operators from left to right
+      while ops:
+        apply_op_from_left(ops, values)
+      val, _ = values.pop()
+      return val, it
+    
+    result, _ = sub_evaluate(factors)
+    return result
 
   def has_one_operator(self, expression):
-    """
-    This function check whether has allowed operator in expression.
-    """
+    """This function check whether has allowed operator in expression."""
     if len(expression)==0:
       return False
     ops = self.operators.keys()
@@ -101,9 +163,9 @@ class Calculator:
     return False
 
   def has_continue_operator(self, expression):
-    """
-    This function check whether next string is allowed operator.
-    """
+    """This function check whether next string is allowed operator."""
+    if expression[0] in ('(', ')'):
+      return True
     if expression[0] not in self.operators.keys():
       return False
     return True
@@ -111,8 +173,9 @@ class Calculator:
   def start(self):
     """
     examples for input expression:
-    - "eg1: 1(2x4)5 = [!]Invalid input: number following group without operator!"
-    - "eg2: 11+(99x(89+1)/2) = 4466"
+    - eg1: 1(2x4)5 = Invalid input: number '5' follows group without operator!
+    - eg2: 11+(99x(89+1)/2) = 4466
+    - eg3: 14+(1+4%3)-2 = 14
     """
     result = ""
     while True:
@@ -125,10 +188,9 @@ class Calculator:
         evalText = f"{result}{evalText}"
         try:
           result = self.evaluate(evalText)
-          result = int(result) if int(result) == result else result
           print(f'[o]Result: {evalText} = {result}')
         except Exception as e:
-          print(f"[!]{e}")
+          print(f"[!]{e}",)
       if input('[?]Do you wish to continue calculate base on the current result? [y(es)/n(o)]: ').strip().lower() not in ('yes', 'y'):
         break
 
